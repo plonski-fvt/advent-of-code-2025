b6abe4f2a8ebfb482473fcdadceedf6ac973a131
Patrick A Plonski <pahplonski@gmail.com>
2025-12-14 19:31:39 -0600

    f

--- day03/day03b.py
+++ day03/day03b.py
@@ -1,30 +1,20 @@
 sum = 0
 
 def find_highest_number(digits: list[int], number_power: int):
-    
     # get everything tied for max that can be the highest
     best_digit = 0
-    best_indices = []
+    best_index = 0
     for index, value in enumerate(digits[0:len(digits) - number_power]):
         if value > best_digit:
             best_digit = value
-            best_indices = [index]
-        elif value == best_digit:
-            best_indices.append(index)
+            best_index = index
 
     if number_power == 0:
         return best_digit
 
-    highest_number = 0
-    for index in best_indices:
-        candidate_number = best_digit * 10 ** number_power + find_highest_number(digits[index+1:], number_power - 1)
-        if candidate_number > highest_number:
-            highest_number = candidate_number
-
-    return highest_number
+    candidate_number = best_digit * 10 ** number_power + find_highest_number(digits[best_index+1:], number_power - 1)
+    return candidate_number
         
-
-
 
 with open("input.txt") as f:
     lines = f.readlines()
